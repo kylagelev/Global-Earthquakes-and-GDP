5255a2b7913f7ce1aecc0b33bf4db59786516f1a
Aaron Karpie <aaronkarpie@gmail.com>
2021-04-15 22:02:43 -0700

    test

---
+++ app.js
@@ -0,0 +1,36 @@
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 500;
+
+// Define the chart's margins as an object
+var margin = {
+  top: 60,
+  right: 60,
+  bottom: 60,
+  left: 60
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - margin.left - margin.right;
+var chartHeight = svgHeight - margin.top - margin.bottom;
+
+// Select body, append SVG area to it, and set its dimensions
+var svg = d3.select("body")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append a group area, then set its margins
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Configure a parseTime function which will return a new Date object from a string
+var parseTime = d3.timeParse("%B");
+
+
+d3.csv("GDP_data.csv").then(function(gdpData){
+console.log(gdpData)
+
+console.log(gdpData[0][2000])
+
+})
