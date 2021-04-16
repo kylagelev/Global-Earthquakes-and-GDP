d8244ee418daf016407608d7b0487ca6f2192875
Aaron Karpie <aaronkarpie@gmail.com>
2021-04-16 11:44:10 -0700

    adding initial line chart code

--- app.js
+++ app.js
@@ -28,9 +28,74 @@ var chartGroup = svg.append("g")
 var parseTime = d3.timeParse("%B");
 
 
-d3.csv("GDP_data.csv").then(function(gdpData){
+d3.csv("Data/GDP_IDN.csv").then(function(gdpData){
 console.log(gdpData)
 
-console.log(gdpData[0][2000])
+// gdpData.forEach(function(data) {
+//   data.Year =  parseTime(data.Year);
+// });
 
+  var xTimeScale = d3.scaleLinear()
+  .range([0,chartWidth])
+  .domain(d3.extent(gdpData, data=>data.Year))
+
+  var yLinearScale =d3.scaleLinear()
+    .range([chartHeight,0])
+    .domain([0,d3.max(gdpData,data=>data.GDPgrowth)])
+
+  
+
+  var bottomAxis = d3.axisBottom(xTimeScale);
+  var leftAxis = d3.axisLeft(yLinearScale)
+
+  var drawLine = d3
+  .line()
+  .x(data => xTimeScale(data.Year))
+  .y(data => yLinearScale(data.GDPgrowth));
+
+  chartGroup.append("path")
+  // The drawLine function returns the instructions for creating the line for milesData
+  .attr("d", drawLine(gdpData))
+  .attr("fill", "gray")
+  .classed("line", true);
+
+chartGroup.append("g")
+    .classed("axis", true)
+    .attr("transform", "translate(0, " + chartHeight + ")")
+    .call(bottomAxis);
+
+chartGroup.append("g")
+    .classed("axis", true)
+    .call(leftAxis);
+
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data([gdpData])
+    .enter()
+    .append("circle")
+    .attr("r", "10")
+    .attr("fill", "red");
+  
+    circlesGroup.on("mouseover", function() {
+      d3.select(this)
+        .transition()
+        .duration(1000)
+        .attr("r", 20)
+        .attr("fill", "lightblue");
+    })
+
+    .on("mouseout", function() {
+      d3.select(this)
+        .transition()
+        .duration(1000)
+        .attr("r", 10)
+        .attr("fill", "red");
+    });
+  
+    chartGroup.selectAll("circle")
+    .transition()
+    .duration(1000)
+    .attr("cx", data => xTimeScale(data.Year))
+    .attr("cy", data => yLinearScale(data.GDPgrowth));
 })
+
+
