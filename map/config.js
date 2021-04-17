78678aec2bcd5f8f111a65bc1b99a0d47d14ef69
Kyla <75227767+kylagelev@users.noreply.github.com>
2021-04-16 18:40:46 -0700

    map information
    just setting up file, so I don't mess things up

---
+++ map/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoia3lsYWdlbGV2IiwiYSI6ImNrbjF6b3RmNzBzOGsydXFwaXplajU3amcifQ.--GxVlzMIA4n03YABveKKw"
---
+++ map/kg_app.js
@@ -0,0 +1,15 @@
+var myMap = L.map("map", {
+    center: [45.52, -122.67],
+    zoom: 13
+  });
+  
+  // Adding a tile layer (the background map image) to our map
+  // We use the addTo method to add objects to our map
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
