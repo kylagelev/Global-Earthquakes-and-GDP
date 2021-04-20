0ab0c2f2dea076b067f9c2c24ed95138c6766ac6
Kyla <75227767+kylagelev@users.noreply.github.com>
2021-04-20 16:32:15 -0700

    working on flask app
    still having trouble with the link to the database side of things

--- Flask/flask_app.py
+++ Flask/flask_app.py
@@ -4,6 +4,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+from datetime import datetime, timedelta
 
 from flask import Flask, jsonify
 
@@ -18,9 +19,29 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    print("Map")
-    session = Session(engine)
-    results = session.query(measurement.Longitude, quake.Latitude).all()
-    session.close()
+        print("Map")
+        session = Session(engine)
+        results = session.query(quake.Place, quake.Longitude, quake.Latitude, quake.Magnitude, quake.Converted_Time_GMT).all()
+        session.close()
 
-    return(f'Map')
+        return(f'Map')
+
+# @app.route('/api/v1.0/Indonesia')
+# def Indonesia():
+#         session = Session(engine)
+#         results = session.query(.date, measurement.prcp).all()
+
+#         prcp_dates = []
+#         for date, prcp in results:
+#             prcp_dict = {}
+#             prcp_dict["date"] = date
+#             prcp_dict["prcp"] = prcp
+#             prcp_dates.append(prcp_dict)
+
+#         session.close()
+
+#         return jsonify(prcp_dates)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
