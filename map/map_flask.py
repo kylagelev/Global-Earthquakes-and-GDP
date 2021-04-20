966efbe13685d1b0ee1045bc3d9730617c42712a
Kyla <75227767+kylagelev@users.noreply.github.com>
2021-04-20 00:10:00 -0700

    ok joke's on me
    so I could not get the app to run via python, but it works via jupyter so idk what is happening??/

--- map/map_flask.py
+++ map/map_flask.py
@@ -1,22 +1,22 @@
-import sqlite3
+import numpy as np
 
-import click
-from flask import current_app, g
-from flask.cli import with_appcontext
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from datetime import datetime, timedelta
 
-def get_db():
-    if 'earthquake.db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
+from flask import Flask, jsonify
 
-    return g.db
+database_path = 'test.db'
+engine = create_engine(f'sqlite:///{database_path}')
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+quake = Base.classes.quake
+session = Session(engine)
 
+app = Flask(__name__)
 
-def close_db(e=None):
-    db = g.pop('db', None)
-
-    if db is not None:
-        db.close()
+@app.route("/")
+def home():
+    print("Map")
