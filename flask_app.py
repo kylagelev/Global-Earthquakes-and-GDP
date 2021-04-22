ee1211adf187318703fb07a6b82526e2689a2065
Ken Yeh <yehitsken@gmail.com>
2021-04-21 23:28:10 -0700

    fixed file routes

--- flask_app.py
+++ flask_app.py
@@ -69,10 +69,10 @@ def CHL10():
         
         return render_template('CHL10.html')
 
-# @app.route("/api/v1.0/depth")
-# def depth():
+@app.route("/api/v1.0/depth")
+def depth():
         
-#         return render_template('depth.html')
+        return render_template('depth.html')
 
 @app.route("/api/v1.0/heat_map")
 def heat_map():
--- static/js/app(2012).js
+++ static/js/app(2012).js
@@ -223,7 +223,7 @@ function init() {
   
   if (dataset === 'dataset1') {
     
-  d3.csv("./Data_Cleaning/GDP_csvs/GDP_IDN.csv").then(function(gdpData){
+  d3.csv("../Data_Cleaning/GDP_csvs/GDP_IDN.csv").then(function(gdpData){
   console.log(gdpData)
   
   gdpData.forEach(function(data) {
@@ -356,7 +356,7 @@ function init() {
   }else if (dataset==='dataset2'){
     dataset !== 'dataset1'
     dataset !== 'dataset2'
-    d3.csv("./Data_Cleaning/GDP_csvs/GDP_IDN.csv").then(function(gdpData){
+    d3.csv("../Data_Cleaning/GDP_csvs/GDP_IDN.csv").then(function(gdpData){
       console.log(gdpData)
       
       gdpData.forEach(function(data) {
@@ -483,7 +483,7 @@ function init() {
           .attr("y", data => yLinearScale(data[12].GDP));
       })
   }else{
-    d3.csv("./Data_Cleaning/GDP_csvs/GDP_IDN.csv").then(function(gdpData){
+    d3.csv("../Data_Cleaning/GDP_csvs/GDP_IDN.csv").then(function(gdpData){
       console.log(gdpData)
       
       gdpData.forEach(function(data) {
